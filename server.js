efde19f916e913184ecd5dbfc4b61287d838edbc
YiTongG <gongyitong0201@gmail.com>
2025-07-29 16:36:31 +0800

    feat: add swagger doc

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 require('dotenv').config();
 const express = require('express');
+const expressSwagger = require('express-swagger-generator');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const PORT = 5000;
 
+//swagger doc
+const options = {
+  swaggerDefinition: {
+    info: {
+      title: 'Finance Backend API',
+      version: '1.0.0',
+      description: 'API documentation',
+    },
+    basePath: '/',
+    produces: ['application/json'],
+    schemes: ['http'],
+  },
+  basedir: __dirname,
+  files: ['./routes/*.js'],
+};
+
+expressSwagger(app)(options);
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-
-
 app.get('/', (req, res) => res.send('服务器已启动'));
 
 // Routes
@@ -17,12 +34,13 @@ const transactionRoutes = require('./routes/transactionRoutes');
 const stocksRoutes = require('./routes/stocksRoutes');
 const indexRoutes = require('./routes/indexRoutes');
 const globalRoutes  = require('./routes/globalRoutes')
-
+const swaggerRouter = require('./routes/swagger');
 
 app.use('/api/transactions', transactionRoutes);
 app.use('/api/indexes', indexRoutes);
 app.use('/api/stocks', stocksRoutes);
 app.use('/api/global', globalRoutes);
+app.use('/api-docs', swaggerRouter);
 
 
 // Start server
