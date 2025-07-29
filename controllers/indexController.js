1c6828664f172b4a30fda82f25549747f29d0aa6
YiTongG <gongyitong0201@gmail.com>
2025-07-29 12:03:42 +0800

    feat: chart on main page and set up commitlint

---
+++ controllers/indexController.js
@@ -0,0 +1,34 @@
+// controllers/indexController.js
+   
+const IndexModel = require('../models/indexModel'); 
+
+// 获取大盘指数
+exports.getIndexes = async (req, res) => {
+    try {
+        const data = await StockModel.fetchIndexes();
+        res.json(data); 
+    } catch (error) {
+        res.status(500).json({ message: '获取指数数据失败', error: error.message });
+    }
+};
+
+// The function name should match what's in the router
+exports.fetchChartInfo = async (req, res) => {
+    try {
+        const { ticker } = req.params;
+        const data = await IndexModel.fetchChartStats(ticker);
+        
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ message: '获取指数详细信息失败', error: error.message });
+    }
+};
+// The function name should match what's in the router
+exports.fetchRegionTrending = async (req, res) => {
+    try {
+        const data = await IndexModel.getTrendingDistribution();
+        res.json(data); 
+    } catch (error) {
+        res.status(500).json({ message: '获取指数趋势分布信息失败', error: error.message });
+    }
+};
--- server.js
+++ server.js
@@ -9,14 +9,21 @@ const PORT = 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+
+
 app.get('/', (req, res) => res.send('服务器已启动'));
 
 // Routes
 const transactionRoutes = require('./routes/transactionRoutes');
 const stocksRoutes = require('./routes/stocksRoutes');
+const indexRoutes = require('./routes/indexRoutes');
+
 
 app.use('/api/transactions', transactionRoutes);
+app.use('/api/indexes', indexRoutes);
+
 app.use('/api/stocks', stocksRoutes);
+
 
 // Start server
 app.listen(PORT, () => {
