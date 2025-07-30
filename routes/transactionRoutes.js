5a139225cac25cfad12b24c8920f27e7e910f974
“wangjiahao” <1356244249@qq.com>
2025-07-30 14:52:09 +0800

    feat: add personal asset information related modules

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,16 +1,13 @@
+
+
 const express = require('express');
 const router = express.Router();
 
-const {
-  getAllTransactions,
-  createTransaction,
-  updateTransaction,
-  deleteTransaction
-} = require('../controllers/transactionController');
+const   transactionController= require('../controllers/transactionController');
 
-router.get('/', getAllTransactions);
-router.post('/', createTransaction);
-router.put('/:id', updateTransaction);
-router.delete('/:id', deleteTransaction);
+// 定义UserInformation API端点
+
+///api/transactions/user/45430196
+router.get('/user/:UserID', transactionController.getUserStockInformation);
 
 module.exports = router;
